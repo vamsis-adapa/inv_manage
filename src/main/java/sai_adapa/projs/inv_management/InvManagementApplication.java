a90303cca7162753fcad1fbab252b5c557f7eef4
Vamsi Adapa <vamsis.adapa@gmail.com>
2021-02-11 16:00:49 -0500

    Initial Commit
    Scratch project with maven dependencies

---
+++ src/main/java/sai_adapa/projs/inv_management/InvManagementApplication.java
@@ -0,0 +1,13 @@
+package sai_adapa.projs.inv_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sai_adapa/projs/inv_management/InvManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package sai_adapa.projs.inv_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
