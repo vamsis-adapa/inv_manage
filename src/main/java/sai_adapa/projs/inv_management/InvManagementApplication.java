ba839423944b2b0e79f33c8697df3dcf38f1a080
Vamsi Adapa <vamsis.adapa@gmail.com>
2021-02-19 08:28:35 -0500

    implements 3rd party http logger

--- src/main/java/sai_adapa/projs/inv_management/InvManagementApplication.java
+++ src/main/java/sai_adapa/projs/inv_management/InvManagementApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.zalando.logbook.Logbook;
 import sai_adapa.projs.inv_management.auth.AuthTools;
 import sai_adapa.projs.inv_management.item.Item;
 import sai_adapa.projs.inv_management.item.ItemRepository;
@@ -28,77 +29,13 @@ public class InvManagementApplication {
 
 
 
-////    Test bed
-//
-//        @Autowired
-//        UsersService usersService;
-//    VendorRepository vendorRepository;
-//    AdminRepository adminRepository;
-//    UsersRepository usersRepository;
-//    ItemRepository itemRepository;
-//    StockRepository stockRepository;
-//
-//    @Autowired
-//    protected void setStockRepository(StockRepository stockRepository) {
-//        this.stockRepository = stockRepository;
-//    }
-//
-//    @Autowired
-//    protected void setItemRepository(ItemRepository itemRepository) {
-//        this.itemRepository = itemRepository;
-//    }
-//
-//    @Autowired
-//    protected void getVendorRepository(VendorRepository vendorRepository) {
-//        this.vendorRepository = vendorRepository;
-//    }
-//
-//    @Autowired
-//    protected void getAdminRepository(AdminRepository adminRepository) {
-//        this.adminRepository = adminRepository;
-//    }
-//
-//    @Autowired
-//    protected void getUsersRepository(UsersRepository usersRepository) {
-//        this.usersRepository = usersRepository;
-//    }
-//
-//
+
     @EventListener(ApplicationReadyEvent.class)
     public void doSomethingAfterStartup() {
-//
-//        System.out.println(AuthTools.encodePassword("printer"));
-//
-//        PreUsers preUsers = new PreUsers("dam","lef","coee","hard");
-//
-//        usersService.addUser(preUsers);
-//        usersService.addUser("choc","draw","kdjf;lkdjf fdlakj; ", "meow");
-//
-//
-//        System.out.println(usersService.verifyUser("draw","meow"));
-//        System.out.println(usersService.verifyUser("draw","myadf"));
-//        Users k = usersService.getUser("draw");
-//        System.out.println(usersService.createSession(k));
-//        usersService.endSession(k);
-//        System.out.println("Strawberry");
 
-
+        Logbook logbook = Logbook.create();
     }
 
-//        Vendor vendor = new Vendor("vend", "straw", "cjhp", "my", "yare");
-//        vendorRepository.save(vendor);
-//
-//        Admin admin = new Admin("admi", "dam", "choc", "straw");
-//        adminRepository.save(admin);
-//
-//        Users users = new Users("thuser", "choc", "prince", "hot", "king");
-//        usersRepository.save(users);
-//
-//        Item item = new Item("meow_item", "damn", "hard");
-//        itemRepository.save(item);
-//
-//        Stock stock = new Stock("calc_meow",vendor,item,3,new java.util.Date());
-//        stockRepository.save(stock);
 
 
 }
