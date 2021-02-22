294d6ba9846ca43885a993b9ae39f51ffacccd83
Vamsi Adapa <vamsis.adapa@gmail.com>
2021-02-22 00:00:44 -0500

    Removes unused imports in main

--- src/main/java/sai_adapa/projs/inv_management/InvManagementApplication.java
+++ src/main/java/sai_adapa/projs/inv_management/InvManagementApplication.java
@@ -1,24 +1,9 @@
 package sai_adapa.projs.inv_management;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
-import org.zalando.logbook.Logbook;
-import sai_adapa.projs.inv_management.auth.AuthTools;
-import sai_adapa.projs.inv_management.item.Item;
-import sai_adapa.projs.inv_management.item.ItemRepository;
-import sai_adapa.projs.inv_management.item.stock.Stock;
-import sai_adapa.projs.inv_management.item.stock.StockRepository;
-import sai_adapa.projs.inv_management.users.admin.Admin;
-import sai_adapa.projs.inv_management.users.admin.AdminRepository;
-import sai_adapa.projs.inv_management.users.user.PreUsers;
-import sai_adapa.projs.inv_management.users.user.Users;
-import sai_adapa.projs.inv_management.users.user.UsersRepository;
-import sai_adapa.projs.inv_management.users.user.UsersService;
-import sai_adapa.projs.inv_management.users.vendor.Vendor;
-import sai_adapa.projs.inv_management.users.vendor.VendorRepository;
 
 @SpringBootApplication
 public class InvManagementApplication {
@@ -28,14 +13,11 @@ public class InvManagementApplication {
     }
 
 
-
-
     @EventListener(ApplicationReadyEvent.class)
     public void doSomethingAfterStartup() {
 
-        Logbook logbook = Logbook.create();
-    }
 
+    }
 
 
 }
