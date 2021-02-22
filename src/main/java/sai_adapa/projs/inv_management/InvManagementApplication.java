401f7633584dea5434df2d1ae4ac48e9cb8b0b0f
Vamsi Adapa <vamsis.adapa@gmail.com>
2021-02-22 00:16:44 -0500

    adds some cache configuration

--- src/main/java/sai_adapa/projs/inv_management/InvManagementApplication.java
+++ src/main/java/sai_adapa/projs/inv_management/InvManagementApplication.java
@@ -3,9 +3,11 @@ package sai_adapa.projs.inv_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
+@EnableCaching
 public class InvManagementApplication {
 
     public static void main(String[] args) {
