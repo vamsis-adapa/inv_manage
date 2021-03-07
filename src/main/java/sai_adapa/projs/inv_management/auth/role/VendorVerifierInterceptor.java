ba1e863c22066b4ee84a022934162ba3240aeb65
Vamsi Adapa <vamsis.adapa@gmail.com>
2021-03-07 07:38:33 +0530

    updates vendor service to use sessionIdentity

--- src/main/java/sai_adapa/projs/inv_management/auth/role/VendorVerifierInterceptor.java
+++ src/main/java/sai_adapa/projs/inv_management/auth/role/VendorVerifierInterceptor.java
@@ -3,6 +3,8 @@ package sai_adapa.projs.inv_management.auth.role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import sai_adapa.projs.inv_management.auth.identity.SessionIdentity;
+import sai_adapa.projs.inv_management.model.users.Vendor;
 import sai_adapa.projs.inv_management.services.VendorService;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,7 +14,12 @@ import java.io.IOException;
 @Component
 public class VendorVerifierInterceptor implements HandlerInterceptor {
     VendorService vendorService;
+    SessionIdentity sessionIdentity;
 
+    @Autowired
+    public void setSessionIdentity(SessionIdentity sessionIdentity) {
+        this.sessionIdentity = sessionIdentity;
+    }
 
     @Autowired
     public void setVendorService(VendorService vendorService) {
@@ -22,13 +29,18 @@ public class VendorVerifierInterceptor implements HandlerInterceptor {
 
     @Override ///Todo: add error resp in case of failure
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        if (vendorService.verifySession(request.getHeader("session_token")))
-            return true;
+        Vendor vendor = vendorService.getUserBySession(request.getHeader("session_token")) ;
+        if (vendor!= null) {
+            sessionIdentity.setIdentity(vendor.getEmail());
+            return  true;
+
+        }
         try {
             response.getWriter().write("You are not a vendor");
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         response.setStatus(401);
         return false;
     }
